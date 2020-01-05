52d8a02ebb5254a5475addc65a412551fa275ad8
Parker Egli <parker.egli22@gmail.com>
2020-01-05 13:40:17 -0600

    Setting up mongoose

--- packages/api/app.js
+++ packages/api/app.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://clonebayAPP:2La8CsoCKvu5jrsT@clonebaydb-sumpy.gcp.mongodb.net/test?retryWrites=true&w=majority", {dbName : 'clonebayDB'});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
