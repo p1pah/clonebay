53e03e6b18dac8fdaeda891f1d4edc35dd4bf28d
SloshyWorm <adityabhasin8@gmail.com>
2020-01-05 17:58:12 -0600

    initial front end

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react'
-import logo from './logo.svg'
+import styled from 'styled-components'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+
+  const Title = styled.h1`
+  font-size: 3em;
+  text-align: center;
+  color: blueviolet;
+`;
+
+const Wrapper = styled.section`
+  padding: 2em;
+  background: papayawhip;
+`;
+
+return(
+  <Wrapper>
+    <Title>
+     BeatsBay
+    </Title>
+  </Wrapper>
+);
 }
 
 export default App
+  
