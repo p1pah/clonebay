14c407802c4d80c9c0622db8e27232f5194ce40a
ben-hapip <benhapip@gmail.com>
2020-01-05 14:38:49 -0600

    moved packages

--- packages/api/app.js
+++ packages/api/app.js
@@ -5,13 +5,13 @@ require('dotenv').config()
 const app = express()
 app.use(express.json())
 
-mongoose.connect(process.env.MONGO_DB_URI, {dbName : 'clonebayDB'});
+mongoose.connect(process.env.MONGO_DB_URI, { dbName: 'clonebayDB' })
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
-  console.log('Database connected');
-});
+  console.log('Database connected')
+})
 
 const port = 3000
 
