44d011179081ca26b4ca5ce644167f880766a26a
ben-hapip <benhapip@gmail.com>
2020-01-18 14:37:28 -0600

    hehe

--- packages/web/src/pages/index.js
+++ packages/web/src/pages/index.js
@@ -10,40 +10,41 @@ import { client } from "../apollo/client"
 const IndexPage = () => {
   console.log(client)
   // let me get them users pls tehe
-  // client
-  //   .query({
-  //     query: gql`
-  //       {
-  //         users {
-  //           email
-  //           password
-  //         }
-  //       }
-  //     `,
-  //   })
-  //   .then(data => console.log(data))
-  // // just a little ability to add via graphql
-  // client
-  //   .mutate({
-  //     mutation: gql`
-  //       mutation {
-  //         createUser(input: { email: "weeeee@weeeee.com", password: "uoeno" }) {
-  //           email
-  //           password
-  //         }
-  //       }
-  //     `,
-  //   })
-  //   .then(data => console.log(data))
+  client
+    .query({
+      query: gql`
+        {
+          users {
+            email
+            password
+          }
+        }
+      `,
+    })
+    .then(data => console.log(data))
+  // just a little ability to add via graphql
+  client
+    .mutate({
+      mutation: gql`
+        mutation {
+          createUser(input: { email: "weeeee@weeeee.com", password: "uoeno" }) {
+            email
+            password
+          }
+        }
+      `,
+    })
+    .then(data => console.log(data))
   return (
     <Layout>
       <h1>Welcome to Clonebay</h1>
-      <p>This site is used to auction things you own to earn that cold hard cash ༼ つ ◕_◕ ༽つ</p>
+      <p>
+        This site is used to auction things you own to earn that cold hard cash
+        ༼ つ ◕_◕ ༽つ
+      </p>
       {/* <div style={{ maxWidth: 300px, marginBottom: 1.45rem }}>
     <Image />
   </div> */}
-
-  
     </Layout>
   )
 }
