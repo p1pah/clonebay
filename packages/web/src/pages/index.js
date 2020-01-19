140d0eab216b84c227380d0c2408d6e09b4bf127
ben-hapip <benhapip@gmail.com>
2020-01-19 13:21:11 -0600

    dat user :)

--- packages/web/src/pages/index.js
+++ packages/web/src/pages/index.js
@@ -1,40 +1,23 @@
-import React, { useState, useEffect } from "react"
-import axios from "axios"
-import { Link } from "gatsby"
-import { graphql } from "gatsby"
-import { useQuery } from "@apollo/react-hooks"
-import gql from "graphql-tag"
+import React from "react"
 import Layout from "../components/layout"
 import { client } from "../apollo/client"
 
 const IndexPage = () => {
   console.log(client)
-  // let me get them users pls tehe
-  client
-    .query({
-      query: gql`
-        {
-          users {
-            email
-            password
-          }
-        }
-      `,
-    })
-    .then(data => console.log(data))
-  // just a little ability to add via graphql
-  client
-    .mutate({
-      mutation: gql`
-        mutation {
-          createUser(input: { email: "weeeee@weeeee.com", password: "uoeno" }) {
-            email
-            password
-          }
-        }
-      `,
-    })
-    .then(data => console.log(data))
+  // // let me get them users pls tehe
+  // client
+  //   .query({
+  //     query: gql`
+  //       {
+  //         users {
+  //           email
+  //           password
+  //         }
+  //       }
+  //     `,
+  //   })
+  //   .then(data => console.log(data))
+
   return (
     <Layout>
       <h1>Welcome to Clonebay</h1>
--- packages/web/src/pages/register.js
+++ packages/web/src/pages/register.js
@@ -1,37 +1,72 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
-
+import { useMutation } from "@apollo/react-hooks"
+import gql from "graphql-tag"
 import Layout from "../components/layout"
 
-const SecondPage = () => (
-  <Layout>
-     <div className="col-md-6 col-md-offset-3">
-                <h2>Register</h2>
-                <form >
-                    <div>
-                        <label htmlFor="firstName">First Name</label>
-                        <input type="text" className="form-control" name="firstName"/>
-                    </div>
-                    <div >
-                        <label htmlFor="lastName">Last Name</label>
-                        <input type="text" className="form-control" name="lastName"/>
-                    </div>
-                    <div>
-                        <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control" name="username"/>
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" />
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary">Register</button>
-                    </div>
-                </form>
-            </div>
+// just a little ability to add via graphql
+const CREATE_USER = gql`
+  mutation CreateUser($email: String!, $password: String!) {
+    createUser(input: { email: $email, password: $password }) {
+      email
+      password
+    }
+  }
+`
 
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const onEmailChange = e => {
+    e.preventDefault()
+    setEmail(e.target.value)
+  }
+  const onPWChange = e => {
+    e.preventDefault()
+    setPassword(e.target.value)
+  }
+  const [createUser] = useMutation(CREATE_USER)
+
+  return (
+    <Layout>
+      <div>
+        <h2>Register</h2>
+        <form>
+          <div>
+            <label type="text">Email</label>
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              onChange={onEmailChange}
+            />
+          </div>
+          <div>
+            <label type="text">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              onChange={onPWChange}
+            />
+          </div>
+          <div className="form-group">
+            <button
+              onClick={e => {
+                console.log(password)
+                console.log(email)
+                e.preventDefault()
+                createUser({ variables: { email, password } })
+              }}
+              className="btn btn-primary"
+            >
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </Layout>
+  )
+}
 
 export default SecondPage
